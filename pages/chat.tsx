697cb6103cddf2349376e67fc6ce0a69c9bfd634
chowais181 <f180269@nu.edu.pk>
2023-05-18 20:27:40 +0500

    added

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from 'react';
+import React from 'react';
 import Layout from '../components/layout';
 // import LayoutChat from '../components/layoutChat';
 import { getCookie } from 'cookies-next';
